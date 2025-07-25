af9124b11c149bba36faa68b927bfac01087c58e
ggjskjd <layan12rz@gmail.com>
2025-07-25 21:36:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 
 // ✅ Serve static files (including saved photos)
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "Untitled-1.html"));
